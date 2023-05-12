f1ba2aac14714728c86d9f6bf90c18a873a5cc1d
Adam Krawczyk <adam.krawczyk@robotec.ai>
2023-05-12 12:39:29 +0200

    Console error
    Signed-off-by: Adam Krawczyk <adam.krawczyk@robotec.ai>

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    console.log('Running test action')
+    // console.log('Running test action')
+    window.console.log('Running test action')
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
