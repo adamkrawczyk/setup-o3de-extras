ae814fe8fcd8ec0beb5982c40c8e9b34dfdc8640
Adam Krawczyk <adam.krawczyk@robotec.ai>
2023-06-01 10:25:11 +0200

    Mount in different path
    Signed-off-by: Adam Krawczyk <adam.krawczyk@robotec.ai>

--- src/main.ts
+++ src/main.ts
@@ -45,6 +45,9 @@ async function runContainerScript(imageName: string, scriptToExecute: string): P
   const tempFilePath = path.join(os.tmpdir(), 'ci-test');
   const tempFileName = 'script.sh';
   const tempFileFullPath = path.join(tempFilePath, tempFileName);
+  const containerScriptsPath = path.join(os.tmpdir(), 'scripts');
+  const containerScriptFullPath = path.join(containerScriptsPath, tempFileName);
+
 
   try {
     // try to remove the file asynchronously
@@ -84,32 +87,33 @@ async function runContainerScript(imageName: string, scriptToExecute: string): P
     if (!isFile) {
       console.error(`Created file is not a file: ${tempFileFullPath}`);
       throw new Error(`Created file is not a file: ${tempFileFullPath}`);
-    }
-
-    // Determine the working directory
-    const repoPath = execSync('pwd').toString().trim();
-    console.log(`Working directory: ${repoPath}`);
-
-    // Determine the command based on the repo name
-    let command = '';
-    if (repoPath.endsWith('o3de-extras')) {
-      console.log('o3de-extras detected');
-      command = `docker run --rm -v ${tempFileFullPath}:${tempFileFullPath} -v ${repoPath}:/data/workspace/o3de-extras --workdir /data/workspace/o3de-extras ${imageName} /bin/bash ${tempFileFullPath}`;
     } else {
-      console.log(`Running on a general-purpose repo: ${repoPath}`);
-      command = `docker run --rm -v ${tempFileFullPath}:${tempFileFullPath} -v ${repoPath}:/data/workspace/repository --workdir /data/workspace/repository ${imageName} /bin/bash ${tempFileFullPath}`;
+
+      // Determine the working directory
+      const repoPath = execSync('pwd').toString().trim();
+      console.log(`Working directory: ${repoPath}`);
+
+      // Determine the command based on the repo name
+      let command = '';
+      if (repoPath.endsWith('o3de-extras')) {
+        console.log('o3de-extras detected');
+        command = `docker run --rm -v ${tempFilePath}:${containerScriptsPath} -v ${repoPath}:/data/workspace/o3de-extras --workdir /data/workspace/o3de-extras ${imageName} /bin/bash ${containerScriptFullPath}`;
+      } else {
+        console.log(`Running on a general-purpose repo: ${repoPath}`);
+        command = `docker run --rm -v ${tempFilePath}:${containerScriptsPath} -v ${repoPath}:/data/workspace/repository --workdir /data/workspace/repository ${imageName} /bin/bash ${containerScriptFullPath}`;
+      }
+
+      // Execute the Docker command using spawnSync
+      const result = spawnSync('sh', ['-c', command]);
+
+      if (result.error) {
+        console.error('Command execution failed:', result.error);
+        throw result.error;
+      }
+
+      const output = result.stdout.toString();
+      return output;
     }
-
-    // Execute the Docker command using spawnSync
-    const result = spawnSync('sh', ['-c', command]);
-
-    if (result.error) {
-      console.error('Command execution failed:', result.error);
-      throw result.error;
-    }
-
-    const output = result.stdout.toString();
-    return output;
   } catch (error) {
     console.error(error);
     throw error;
